8970910ee6eecc7192481e773da4a7cf50bb3169
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-06-26 05:49:02 +0900

    Added header fixed in the middle (with bug)

--- js/mainPage.js
+++ js/mainPage.js
@@ -3,14 +3,17 @@
 
   /*変数*/
   var pageTop_link = document.getElementById('pageTop_link'); //トップに戻るリンクを代入
+  var header = document.getElementById('header'); //ヘッダーを代入
 
   /*トップに戻るリンクの表示非表示切り替え*/
   function height() {
     var y = window.scrollY; //スクロール高さを代入
-    if (y >= 300) { //300位上時にのみ表示
+    if (y >= 500) { //300位上時にのみ表示
       pageTop_link.classList.add('show');
+      header.classList.add('fixed');
     } else {
       pageTop_link.classList.remove('show');
+      header.classList.remove('fixed');
     }
   }
   setInterval(height, 10); //10msに一回"height"関数を実行
