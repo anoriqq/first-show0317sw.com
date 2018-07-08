44a59e283b490a1ffb490e56d750b77f46cc9f14
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-07-09 03:10:36 +0900

    Adding a header image and making the main article separate file.

--- js/iframe.js
+++ js/iframe.js
@@ -2,31 +2,6 @@
   'use strict';
 
   /*変数*/
-  var pageTop_link = document.getElementById('pageTop_link'); //トップに戻るリンクを取得
-  var header = document.getElementById('header'); //ヘッダーを取得
-
-  /*トップに戻るリンクの表示非表示切り替え関数*/
-  function height() {
-    var y = window.scrollY; //スクロール高さを代入
-    if (y >= 500) { //300位上時にのみ表示
-      pageTop_link.classList.add('show');
-      header.classList.add('fixed');
-    } else {
-      pageTop_link.classList.remove('show');
-      header.classList.remove('fixed');
-    }
-  }
-  setInterval(height, 10); //10msに一回"height"関数を実行
-
-  /*トップに戻るリンクの動作処理関数*/
-  pageTop_link.onclick = function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    })
-  }
-
-  /*変数*/
   var elm = document.getElementById("main-body"); //メインの内容を取得
 
   // 子画面のコンテンツサイズに合わせてサイズを変更する関数
