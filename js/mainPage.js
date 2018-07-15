fc9520628f3d7f84de7bb98871e9dfd6d008910e
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-07-15 18:15:44 +0900

    Automatic height change script test of iframe

--- js/mainPage.js
+++ js/mainPage.js
@@ -26,31 +26,6 @@
     })
   }
 
-  /*変数*/
-  var elm = document.getElementById("main-body"); //メインの内容を取得
-
-  // 子画面のコンテンツサイズに合わせてサイズを変更する関数
-  function changeParentHeight() {
-    elm.style.height = elm.contentWindow.document.body.scrollHeight + 30 + "px";
-  }
-
-  /*親画面 iframe の高さを変更するイベント*/
-  // 1. 子画面の読み込み完了時点で処理を行う
-  elm.contentWindow.onload = function () {
-    changeParentHeight();
-  };
-
-  // 2. 子画面のウィンドウサイズ変更が完了した時点で処理を行う
-  var timer = 0;
-  elm.contentWindow.onresize = function () {
-    if (timer > 0) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(function () {
-      changeParentHeight();
-    }, 100);
-  };
-
   //ローダー
   document.body.onload = function () {
     const element = document.getElementById('loader');
