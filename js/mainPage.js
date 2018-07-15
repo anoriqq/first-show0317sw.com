aecb2f54160c2389695105da1ab583629743b2c4
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-07-15 22:04:55 +0900

    Corresponding to automatic height acquisition of sidebar

--- js/mainPage.js
+++ js/mainPage.js
@@ -26,10 +26,36 @@
     })
   }
 
-  //ローダー
+  /*ローダー*/
   document.body.onload = function () {
     const element = document.getElementById('loader');
     element.style.display = 'none';
+  };
+
+  /*サイドバー高さ自動変更*/
+  // 子画面の要素を取得
+  var elm = document.getElementById("sideMenu");
+
+  // 子画面のコンテンツサイズに合わせてサイズを変更する関数
+  function changeParentHeight() {
+    elm.style.height = elm.contentWindow.document.body.scrollHeight + "px";
+  }
+
+  // 親画面 iframe の高さを変更するイベント
+  // 1. 子画面の読み込み完了時点で処理を行う
+  elm.contentWindow.onload = function () {
+    changeParentHeight();
+  };
+
+  // 2. 子画面のウィンドウサイズ変更が完了した時点で処理を行う
+  var timer = 0;
+  elm.contentWindow.onresize = function () {
+    if (timer > 0) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      changeParentHeight();
+    }, 100);
   };
 
 })();
