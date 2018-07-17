5126e509526e31dc2388500424ce4a5e8aff8f85
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-07-18 04:28:39 +0900

    Fixed bug of automatic height adjustment function

--- js/autoHeight_sideMenu.js
+++ js/autoHeight_sideMenu.js
@@ -1,24 +1,24 @@
 (function () {
   'use strict';
 
-  /*フッター高さ自動変更*/
+  /*サイドバー高さ自動変更*/
   // 子画面の要素を取得
-  var footer = document.getElementById("footer");
+  var sideMenu = document.getElementById("sideMenu");
 
   // 子画面のコンテンツサイズに合わせてサイズを変更する関数
   function changeParentHeight() {
-    footer.style.height = footer.contentWindow.document.body.scrollHeight + "px";
+    sideMenu.style.height = sideMenu.contentWindow.document.body.scrollHeight + 10 + "px";
   }
 
   // 親画面 iframe の高さを変更するイベント
   // 1. 子画面の読み込み完了時点で処理を行う
-  footer.contentWindow.onload = function () {
+  sideMenu.contentWindow.onload = function () {
     changeParentHeight();
   };
 
   // 2. 子画面のウィンドウサイズ変更が完了した時点で処理を行う
   var timer = 0;
-  footer.contentWindow.onresize = function () {
+  sideMenu.contentWindow.onresize = function () {
     if (timer > 0) {
       clearTimeout(timer);
     }
--- js/mainPage.js
+++ js/mainPage.js
@@ -33,30 +33,4 @@
     element.classList.add('none');
   };
 
-  /*サイドバー高さ自動変更*/
-  // 子画面の要素を取得
-  var sideMenu = document.getElementById("sideMenu");
-
-  // 子画面のコンテンツサイズに合わせてサイズを変更する関数
-  function changeParentHeight() {
-    sideMenu.style.height = sideMenu.contentWindow.document.body.scrollHeight + 30 + "px";
-  }
-
-  // 親画面 iframe の高さを変更するイベント
-  // 1. 子画面の読み込み完了時点で処理を行う
-  sideMenu.contentWindow.onload = function () {
-    changeParentHeight();
-  };
-
-  // 2. 子画面のウィンドウサイズ変更が完了した時点で処理を行う
-  var timer = 0;
-  sideMenu.contentWindow.onresize = function () {
-    if (timer > 0) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(function () {
-      changeParentHeight();
-    }, 100);
-  };
-
 })();
